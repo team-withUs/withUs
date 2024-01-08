17b0d4b7279303fbe924f0b93d078c9c83fd19d5
Jeon <jeongsh950518@gmail.com>
2024-01-08 17:05:55 +0900

    #23 업데이트(컨트롤러 부분 수정) 과 성공코드 추가

--- src/main/java/com/withus/withus/notice/controller/NoticeController.java
+++ src/main/java/com/withus/withus/notice/controller/NoticeController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +39,8 @@ public class NoticeController {
       @RequestBody NoticeRequestDto requestDto
   ) {
     NoticeResponseDto responseDto = noticeService.updateNotice(noticeId, requestDto);
-    return null;
+    return ResponseEntity.status(ResponseCode.SUCCESS_NOTICE_UPDATE.getHttpStatus())
+        .body(CommonResponse.of(ResponseCode.SUCCESS_NOTICE_UPDATE,responseDto));
 
   }
 
