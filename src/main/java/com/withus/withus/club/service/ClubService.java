1108a0dcd3ef92efac3f58f81a063669ab24c910
Joo-Veloper <psjoo3515@gmail.com>
2024-01-11 10:34:57 +0900

    오류 수정

--- src/main/java/com/withus/withus/club/service/ClubService.java
+++ src/main/java/com/withus/withus/club/service/ClubService.java
@@ -3,20 +3,27 @@ package com.withus.withus.club.service;
 import com.withus.withus.club.dto.ClubRequestDto;
 import com.withus.withus.club.dto.ClubResponseDto;
 
+import com.withus.withus.club.dto.ReportClubRequestDto;
+import com.withus.withus.club.dto.ReportClubResponseDto;
 import com.withus.withus.club.entity.Club;
 
 import com.withus.withus.member.entity.Member;
 
 public interface ClubService {
 
+
     ClubResponseDto createClub(ClubRequestDto clubRequestDto, Member member);
 
     ClubResponseDto getClub(Long clubId);
-    
+
     ClubResponseDto updateClub(Long clubId, ClubRequestDto clubRequestDto, Member member);
+
 
     String deleteClub(Long clubId, Member member);
 
-    ReportClubResponseDto createReportClub(Long clubId, ReportClubRequestDto reportClubRequestDto, Member member);
+    void updateReportClub(Long clubId);
 
+    Club findClubById(Long clubId);
+
+    ReportClubResponseDto createReportClub(Long clubId, ReportClubRequestDto reportClubRequestDto, Member member);
 }
