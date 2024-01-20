bfcb79f671e3333566a5a93dcb7ba8ec0963ceaa
Joo-Veloper <psjoo3515@gmail.com>
2024-01-20 15:42:38 +0900

    paging 전 까지

--- src/main/java/com/withus/withus/club/repository/ClubMemberRepository.java
+++ src/main/java/com/withus/withus/club/repository/ClubMemberRepository.java
@@ -1,16 +1,21 @@
 package com.withus.withus.club.repository;
 
 import com.withus.withus.club.entity.ClubMember;
+
+import java.util.List;
 import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
 
-  Optional<ClubMember> findClubMemberByMemberIdAndClubId(Long memberId, Long clubId );
+    Optional<ClubMember> findClubMemberByMemberIdAndClubId(Long memberId, Long clubId);
 
-  Page<ClubMember> findByMemberId(Long member_id, Pageable pageable);
+    Page<ClubMember> findByMemberId(Long member_id, Pageable pageable);
 
-  boolean existsByMemberIdAndClubId(Long memberId, Long clubId);
+    boolean existsByMemberIdAndClubId(Long memberId, Long clubId);
+
+    List<ClubMember> findByClubId(Long clubId);
 }
