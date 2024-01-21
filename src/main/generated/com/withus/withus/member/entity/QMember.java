b833ef915faab76d732dcd29501dbdc79edb75e9
Yoon <dlstjr9390@gmail.com>
2024-01-21 19:59:29 +0900

    fix : 초대시 호스트인 클럽목록 출력

---
+++ src/main/generated/com/withus/withus/member/entity/QMember.java
@@ -0,0 +1,66 @@
+package com.withus.withus.member.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = 491804446L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final com.withus.withus.global.timestamp.QTimeStamp _super = new com.withus.withus.global.timestamp.QTimeStamp(this);
+
+    public final ListPath<com.withus.withus.club.entity.ClubMember, com.withus.withus.club.entity.QClubMember> clubMemberList = this.<com.withus.withus.club.entity.ClubMember, com.withus.withus.club.entity.QClubMember>createList("clubMemberList", com.withus.withus.club.entity.ClubMember.class, com.withus.withus.club.entity.QClubMember.class, PathInits.DIRECT2);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final StringPath email = createString("email");
+
+    public final StringPath filename = createString("filename");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath imageURL = createString("imageURL");
+
+    public final StringPath introduction = createString("introduction");
+
+    public final BooleanPath isActive = createBoolean("isActive");
+
+    public final StringPath loginname = createString("loginname");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath password = createString("password");
+
+    public final StringPath tag = createString("tag");
+
+    public final StringPath username = createString("username");
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
